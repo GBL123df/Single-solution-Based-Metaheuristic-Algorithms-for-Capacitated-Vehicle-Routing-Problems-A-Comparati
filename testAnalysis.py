b7b18c50b1ad8c3a9b08649e28d15622f11aebf6
GBL123df <giuseppebiagio@gmail.com>
2024-02-15 20:56:58 +0100

    predisposizione ambiente e metodi test

--- testAnalysis.py
+++ testAnalysis.py
@@ -27,6 +27,61 @@ file5 = percorso + "Ghent1.txt"
 file6 = percorso + "P-n101-k4.txt"
 file7 = percorso + "Golden_20.txt"
 
+
+class testAlgorithms:
+
+    def __init__(self, file):  # , num_vehicles):
+        current_file_path = os.path.abspath(__file__)
+        project_root = os.path.abspath(os.path.join(current_file_path, '..'))
+        self.file = os.path.join(project_root, file)
+        self.instance = inst.create_instance_from_file(self.file)
+
+    def executeTest(self,kind,par,reps):
+        self.result = []
+        r = 0
+        while r < reps:
+            if kind == "orTools":
+                self.kind = kind
+                self.par = par
+                self.result.append(ortS.solution_ORTools(self.instance,par[0],par[1],par[2],par[3]))
+            elif kind == "solver":
+                self.kind = kind
+                self.par = par
+                self.result.append(self.instance.compute_sol(par[0],par[1],par[2],par[3],par[4],par[5],par[6]))
+            r+=1
+
+
+    def plot_result(self):
+        self.result.plot_routes(arrow=False)
+
+# class MassTest:
+#
+#     def __init__(self, file,kind,firsts,par,reps = 1):  # , num_vehicles):
+#         self.file = file#list
+#         self.par = par
+#         self.kind = kind
+#         self.firsts = firsts
+#         self.reps = reps
+#
+#         modes = [
+#             '1',
+#             '1bis',
+#             '2',
+#             '2bis',
+#             '3',
+#             '3bis'
+#         ]
+#         if kind == 'solver':
+#             self.tests = []
+#             for mode in enumerate(modes):
+#                 test = testAlgorithms(file)
+#                 test.executeTest(kind,par[0],par[1],par[2],par[3],par[4],par[5],par[6])
+#                 self.tests.append(test)
+#                 test = testAlgorithms(file)
+#                 test.executeTest(kind, par[0], par[1], par[2], par[3], par[4], par[5], par[6])
+#                 self.tests.append(test)
+
+
 class TestBenchmarking(unittest.TestCase):
     def setUp(self,pathFile = file):
         current_file_path = os.path.abspath(__file__)
@@ -67,6 +122,15 @@ class TestBenchmarking(unittest.TestCase):
 
 
 
+    def testtestAlgorithms(self):
+        #par = [10, 2, 20, 10, 2, 2,('2', True)]
+        par = [routing_enums_pb2.FirstSolutionStrategy.AUTOMATIC,routing_enums_pb2.LocalSearchMetaheuristic.AUTOMATIC,2,[]]
+        test = testAlgorithms(self.file)
+        # kind = 'solver'
+        kind = 'orTools'
+        test.executeTest(kind = kind,par = par,reps=5)
+        for t in test.result:
+            print(t.value)
 
 if __name__ == '__main__':
     unittest.main()
